c61a453f9d3e039ec25df6c38bb0ad8ffbbd85f2
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-16 21:40:52 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,37 @@
-class Waiter
+class Waiter 
+    attr_accessor :name, :yrs_experience
+    
+    @@all = []
+    
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
 
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        best_tip_meal = meals.max {|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
+        best_tip_meal.customer   
+    end
+
+    def most_frequent_customer
+        customer_count = {}
+        meals.map do |meal|
+            customer_count[:meal.customer] ||= 0
+            customer_count[:meal.customer] += 1
+        end
+        customer_count.max_by {|key,value| v}
+    end
 end
